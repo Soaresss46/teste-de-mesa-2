06c959b5db77942445b64e5df84e29fe69f60cb5
Matheus <144848303+Soaresss46@users.noreply.github.com>
2023-09-21 16:19:18 -0300

    Add files via upload

---
+++ Problema 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        double i = 1.25;
+        i = i/100;
+        double i2 = 1+i;
+        double c = 3800;
+        double n = 0;
+        Console.WriteLine("os valores resultantes são:");
+
+        while (n<6)
+        {   n++; 
+            double resulti = Math.Pow(i2,n);
+            double m = c*resulti;
+            Console.WriteLine($"montante: {m:C}");
+            Console.Write($"rendimento: ");
+            Console.WriteLine($"{m-c:C}");
+        }
+    }
+}
